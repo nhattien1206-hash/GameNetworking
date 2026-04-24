e6b4c445dc43aafbcd75e73b1bf5073d870447ec
nhattien1206-hash <nhattien1206@icloud.com>
2026-04-24 13:48:13 +0700

    Add files via upload

---
+++ LobbyManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    public Button createRoomButton;
+    public Button joinRoomButton;
+
+    void Start()
+    {
+        // Gắn sự kiện cho nút
+        createRoomButton.onClick.AddListener(CreateRoom);
+        joinRoomButton.onClick.AddListener(JoinRoom);
+
+        // Kết nối Photon Cloud
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    void CreateRoom()
+    {
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = 4;
+        PhotonNetwork.CreateRoom("Room_" + Random.Range(1000, 9999), options);
+    }
+
+    void JoinRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Đã vào phòng: " + PhotonNetwork.CurrentRoom.Name);
+        SceneManager.LoadScene("Play"); // Load sang scene Play
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Không tìm thấy phòng, tạo mới...");
+        CreateRoom();
+    }
+}
+
---
+++ PlayerSpawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using RedRunner;
+using RedRunner.Characters;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints; // danh sách vị trí spawn, gán trong Inspector
+
+    void Start()
+    {
+        // Chỉ spawn khi đã kết nối và vào phòng
+        if (PhotonNetwork.IsConnected && PhotonNetwork.InRoom)
+        {
+            SpawnPlayer();
+        }
+    }
+
+    void SpawnPlayer()
+    {
+        // Chọn vị trí spawn ngẫu nhiên hoặc theo index
+        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+
+        // Instantiate nhân vật từ prefab trong Resources
+        GameObject player = PhotonNetwork.Instantiate("RedRunner", spawnPoint.position, spawnPoint.rotation);
+
+        // Gán nhân vật cho GameManager để quản lý
+        RedRunner.GameManager gm = FindFirstObjectByType<RedRunner.GameManager>();
+        if (gm != null)
+        {
+            gm.SetMainCharacter(player.GetComponent<RedRunner.Characters.RedCharacter>());
+        }
+    }
+}
+
+
+
